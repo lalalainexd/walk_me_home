dd58cbd10419e6999e6369643bd77a86da294b8f
lalalainexd <lalalainexd@gmail.com>
2013-05-05 19:11:44 -0600

    can now create a trip given a duration

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,41 @@
 class Trip < ActiveRecord::Base
+  attr_accessor :duration
   belongs_to :user
 
+  validates_presence_of :user
+  validate :duration_is_greater_than_zero, before: :create
+
+  #validate :expected_end_is_after_created_time
+  after_create :calculate_expected_end_at
+
   def self.create_trip_with_duration user_id, duration
+    user = User.find_by_id(user_id)
+
+    Trip.new.tap do |trip|
+      trip.user = user
+      trip.duration = duration
+      trip.save
+    end
 
   end
+
+  private
+  def duration_is_greater_than_zero
+    if duration && duration <= 0
+      errors[:duration] = "Duration must be greater 0"
+    end
+  end
+
+# def expected_end_is_after_created_time
+#   if expected_end_at <= created_at
+#     errors[:expected_end_at] = "Expected end time cannot be before start time"
+#   end
+# end
+
+ def calculate_expected_end_at
+   self.expected_end_at = created_at + duration
+   #this is janky..
+   save
+ end
+
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -3,17 +3,62 @@ require 'spec_helper'
 describe Trip do
   describe "create_trip_with_duration" do
 
+
     context "with valid params" do
-      it "creates a trip for the user" do
-        user = new_user
-        Trip.create_trip_with_duration(user.id, 
+      let(:user) {new_user}
+      let(:trip) {Trip.create_trip_with_duration(user.id, 2000)}
+
+      it "creates a trip" do
+        expect(trip).to_not be_new_record
       end
-      it "creates a trip with the expect_end_time"
+
+      it "creates a trip for the user" do
+        expect(trip.user).to eq user
+      end
+
+      it "creates a trip with the expected_end_at" do
+        expected_expected_end_at = trip.created_at + 2000
+        expect(Trip.find(trip.id).expected_end_at).to eq expected_expected_end_at
+      end
     end
 
     context "with invalid params" do
-      context "with an invalid user id"
-      context "with invalid duration"
+
+      context "with an invalid user id" do
+        it "is invalid" do
+          trip = Trip.create_trip_with_duration(12324, 2000)
+          expect(trip).to be_invalid
+        end
+
+      end
+
+      context "with invalid duration" do
+
+
+        context "of zero" do
+          let(:trip) { Trip.create_trip_with_duration(new_user.id, 0) }
+
+          it "is invalid" do
+            expect(trip).to be_invalid
+          end
+
+          it "has errors" do
+            expect(trip).to have(1).error_on(:duration)
+          end
+
+        end
+
+        context "less than zero" do
+          let(:trip) { Trip.create_trip_with_duration(new_user.id, -1) }
+          it "is invalid" do
+            expect(trip).to be_invalid
+          end
+
+          it "has errors" do
+            expect(trip).to have(1).error_on(:duration)
+          end
+        end
+      end
     end
   end
 
