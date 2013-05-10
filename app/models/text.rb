a9d9b2ccfb047199a3db5675fdea131685e33702
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-05-10 07:52:23 -0600

    changed out for twilio phone number

--- app/models/text.rb
+++ app/models/text.rb
@@ -11,7 +11,7 @@ class Text < ActiveRecord::Base
     @twilio_client = Twilio::REST::Client.new(twilio_sid, twilio_token)
 
     @twilio_client.account.sms.messages.create(
-      :from => "+15005550006", 
+      :from => "+#{twilio_phone_number}", 
       :to => "+1#{number_to_send_to}",
       :body => "Yo yo yo! Sending a message to you at #{number_to_send_to}")
   end 
