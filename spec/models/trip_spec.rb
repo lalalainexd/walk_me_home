21f264c1ab9f93a3e8c78e3cd340e5a2308553ab
lalalainexd <lalalainexd@gmail.com>
2013-05-10 21:22:24 -0600

    refactored trip spec

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -4,12 +4,14 @@ describe Trip do
 
   describe "start" do
     it "creates a trek with default duration" do
-      now = Time.now
-      Time.stub(:now).and_return(now)
-      trip = Trip.new(default_duration: 60)
-      trek = trip.start
-      expect(trek.started_at).to eq (now)
-      expect(trek.expected_end_at).to eq (now + 60)
+      trek = Trek.new
+      treks = []
+
+      subject.stub(:treks).and_return(treks)
+      treks.should_receive(:build).and_return(trek)
+      trek.should_receive(:start)
+
+      subject.start
     end
   end
 
