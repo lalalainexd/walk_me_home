b6484036db7fca053dd6e62526c7b97573123b88
lalalainexd <lalalainexd@gmail.com>
2013-05-10 21:17:52 -0600

    added test to trek

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -1,13 +1,24 @@
 class Trek < ActiveRecord::Base
   attr_accessible :started_at, :expected_end_at
   belongs_to :trip
+  validate :expected_end_at_is_after_started_at
 
   def start duration
     self.tap do |trek|
-      trek.started_at = Time.now
-      trek.expected_end_at = Time.now + duration
+      time = Time.now
+      trek.started_at = time
+      trek.expected_end_at = time + duration
       save
     end
+  end
+
+  private
+
+  def expected_end_at_is_after_started_at
+    if expected_end_at < started_at
+      errors.add(:expected_end_at, 'Expected end at must be after Started at')
+    end
+
   end
 
 end
--- spec/models/trek_spec.rb
+++ spec/models/trek_spec.rb
@@ -2,5 +2,34 @@ require 'spec_helper'
 
 describe Trek do
 
+  describe "start" do
 
+    before do
+    end
+
+    context "positive duration" do
+      it "set the started_at time" do
+        now = Time.now
+        Time.stub(:now).and_return(now)
+
+        subject.start 10
+        expect(subject.started_at).to eq now
+      end
+
+      it "set the expected_end_at time" do
+        now = Time.now
+        Time.stub(:now).and_return(now)
+
+        subject.start 10
+        expect(subject.expected_end_at).to eq now+10
+      end
+    end
+
+    context "negative duration" do
+      it "has expected_end_at errors" do
+        subject.start -10
+        expect(subject).to have(1).error_on(:expected_end_at)
+      end
+    end
+  end
 end
